925c7203883ed81a379720c0fd856a7dafa69e6e
Manu <manuela_c_guimaraes@estudante.sesisenai.org.br>
2025-02-12 11:53:55 -0300

    criada a classe cone

---
+++ Cone.js
@@ -0,0 +1,19 @@
+export default class Cone{
+    constructor(raio, altura){
+        this.raio = raio
+        this.altura = altura
+
+    }
+
+    calcularGeratriz(){
+        return Math.sqrt((this.raio * this.raio) + (this.altura * this.altura))
+    }
+
+    calcularVolume(){
+        return (Math.PI * this.raio * this.raio * this.altura) / 3
+    }
+
+    calcularAreaTotal(){
+        return((Math.PI * this.raio * this.raio) + (Math.PI * this.raio * this.calcularGeratriz()))
+    }
+}
